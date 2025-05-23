2405fc999c9f380092593c33adb37da6de114a6c
Juan Esteban Trillos <juanestrillosm@gmail.com>
2025-05-22 20:20:11 -0500

    Rain, Huts n Black Room

---
+++ VideoMapping/Assets/EventoVisual.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class EventoVisual
+{
+    public float tiempoActivacion;
+    public GameObject objeto;
+    public bool activar = true;
+}
